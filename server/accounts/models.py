56f29930927574cb1b6604dba8f587596d424d73
thebadfordota <sasha.skrynnik01@gmail.com>
2022-05-24 00:56:14 +0400

    fix db rel

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -9,25 +9,6 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class AdvancedUser(AbstractUser):
-    """
-    Данный класс расширяет поля класса 'AbstractUser'.
-    """
-    about_user = models.CharField(max_length=128, blank=True, verbose_name="О пользователе")
-    image = models.ImageField(null=True, blank=True, verbose_name='Фотография')
-    first_name = models.CharField(max_length=50, verbose_name='Имя')
-    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
-    patronymic = models.CharField(max_length=50, blank=True, verbose_name='Отчество')
-
-    class Meta(AbstractUser.Meta):
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
-
-    def __str__(self):
-        return f'{self.username}'
-        # return f'{self.last_name} {self.first_name}'
-
-
 class StudyGroup(BaseModel):
     name = models.CharField(max_length=50, verbose_name='Название')
 
@@ -38,3 +19,24 @@ class StudyGroup(BaseModel):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class AdvancedUser(AbstractUser):
+    """
+    Данный класс расширяет поля класса 'AbstractUser'.
+    """
+    about_user = models.CharField(max_length=128, blank=True, verbose_name="О пользователе")
+    image = models.ImageField(null=True, blank=True, verbose_name='Фотография')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    patronymic = models.CharField(max_length=50, blank=True, verbose_name='Отчество')
+    group_id = models.ForeignKey(StudyGroup, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Группа')
+
+    class Meta(AbstractUser.Meta):
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f'{self.username}'
+        # return f'{self.last_name} {self.first_name}'
+
